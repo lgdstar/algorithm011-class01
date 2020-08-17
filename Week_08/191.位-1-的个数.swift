cd3655ee87cfa60c7760ffec56bae5c0eea08f48
lgdstar <lgdstaralan@163.com>
2020-08-17 14:31:30 +0800

    位1的个数完善

--- Week_08/191.位-1-的个数.swift
+++ Week_08/191.位-1-的个数.swift
@@ -12,18 +12,18 @@ class Solution {
     时间复杂度：O(1) 。运行时间依赖于数字 n 的位数。由于这题中 n 是一个 32 位数，所以运行时间是 O(1) 的。
     空间复杂度：O(1) 。没有使用额外空间
     */
-    func hammingWeight(_ n: Int) -> Int {
+    func hammingWeight_1(_ n: Int) -> Int {
 
         var x = n
         var count = 0
 
         while x != 0 {
             
-            if x % 2 == 1 {
+            if x % 2 == 1 { // 或使用模1来判断 x & 1 == 1
                 count = count + 1
             }
 
-            x = x / 2
+            x = x >> 1 // x / 2  二进制向右一位，十进制除以2
         }
 
         return count
@@ -46,6 +46,29 @@ class Solution {
 
         return count
     }
+
+    /*
+    3、 手动循环计算 "1"的个数
+
+    时间复杂度：O(1) 。运行时间依赖于数字 n 的位数。由于这题中 n 是一个 32 位数，所以运行时间是 O(1) 的。
+    空间复杂度：O(1) 。没有使用额外空间
+    */
+    func hammingWeight(_ n: Int) -> Int {
+
+        var count = 0
+
+        let str = String(n, radix:2)
+
+        for char in str {
+            
+            if char == "1" {
+
+                count = count + 1
+            }
+        }
+        return count
+    }
+
 }
 // @lc code=end
 
